619c94e38dd59f1417e48041337fb0dc82b8e486
Jose Correia <ze.correia467@gmail.com>
2025-04-25 20:05:05 +0100

    Add site map

--- vite.config.ts
+++ vite.config.ts
@@ -3,12 +3,25 @@ import { fileURLToPath, URL } from 'node:url'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueDevTools from 'vite-plugin-vue-devtools'
-
+import Sitemap from 'vite-plugin-sitemap'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [
     vue(),
     vueDevTools(),
+    Sitemap({
+      hostname: 'https://tripvibes.me',
+      robots: [{ userAgent: '*', allow: '/' }],
+      readable: true,
+      dynamicRoutes: [
+        '/',
+        '/tos',
+        '/privacy-policy',
+        '/terms-of-service',
+        '/about-us',
+        '/contact-us',
+      ],
+    }),
   ],
   resolve: {
     alias: {
